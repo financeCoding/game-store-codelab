daf94a4158a1a63d72cd94fa48b1b68febf01684
ggirou <porude-git@yahoo.fr>
2013-11-27 00:18:06 +0100

    User story 2

--- game_store_codelab/web/game.dart
+++ game_store_codelab/web/game.dart
@@ -6,7 +6,7 @@ class XGame extends PolymerElement {
   bool get applyAuthorStyles => true;
   XGame.created() : super.created();
   
-  Game game = new Game.sample();
+  @published Game game = new Game.sample();
   
   String uppercase(String value) => value.toUpperCase();
   String stars(int rating) => new List.generate(rating, (_) => "\u2605").join();
